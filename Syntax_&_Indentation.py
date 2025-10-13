518dc51750f217facc7c3d2dca5065793cdb7cb3
Umesh Pradip Sanap <umesh2809@Umeshs-MacBook-Air.local>
2025-10-13 20:13:38 +0530

    Commit for Syntax and indentation

---
+++ Syntax_&_Indentation.py
@@ -0,0 +1,7 @@
+#Syntax and Indentatin
+#Syntax
+num = 10        #Syntax for variable declaration
+num- = 5        # syntax is invalid when used symbol in variable.
+
+for i in range(3):
+print(i)        #indentation is necessary here
