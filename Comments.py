5c9dd3a393353f5c7cd94b3635d6ed9241fad7ef
Umesh Pradip Sanap <umesh2809@Umeshs-MacBook-Air.local>
2025-10-13 20:21:36 +0530

    Program for Comments in python

---
+++ Comments.py
@@ -0,0 +1,16 @@
+#comments in python
+# This is a single-line comment
+print("Hello, World!")  # This will print Hello, World!
+
+# This program calculates the area of a rectangle
+# Formula: Area = length * width
+length = 5
+width = 3
+area = length * width
+print("Area of rectangle:", area)
+
+"""
+This program demonstrates
+the use of multi-line comments, using triple quotes
+"""
+print("Learning Python Comments!")
